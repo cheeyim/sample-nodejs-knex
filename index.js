9ec1e298eed4fabf15dc79c02465759ab8d878a5
cheeyim <cheeyim@gmail.com>
2017-07-08 23:29:38 +0800

    login route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
+
 app.post('/createUser', (req, res) => {
   store
     .createUser({
@@ -13,6 +14,19 @@ app.post('/createUser', (req, res) => {
     })
     .then(() => res.sendStatus(200))
 })
+
+app.post('/login', (req, res) => {
+  store
+    .authenticate({
+      username: req.body.username,
+      password: req.body.password
+    })
+    .then(({ success }) => {
+      if (success) res.sendStatus(200)
+      else res.sendStatus(401)
+    })
+})
+
 app.listen(7555, () => {
   console.log('Server running on http://localhost:7555')
 })
